bed38d8658af18866168a577b61be45ec6e517d5
Adhitya Sidiq Permana <adhityasp18@gmail.com>
2019-10-23 12:43:59 +0700

    Insert & Delete

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -4,8 +4,24 @@ class Mahasiswa_model extends CI_model{
 	public function getAllMahasiswa(){
 		return $query = $this->db->get('mahasiswa')->result_array();
 	}
+
+	public function tambahDataMahasiswa(){
+		$data = [
+			"nrp" => $this->input->post('nrp', true),
+			"nama" => $this->input->post('nama', true),
+			"email" => $this->input->post('email', true),
+			"jurusan" => $this->input->post('jurusan', true)
+		];
+
+		$this->db->insert('mahasiswa', $data);
+	}
+
+	public function hapusDataMahasiswa($id){
+		$this->db->where('id', $id);
+		$this->db->delete('mahasiswa');
+	}
 }
 
-
+	
 
  ?>
